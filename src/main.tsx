55103b5e1161ddb6e0ee5a7cfb25c60ca6fd9bf3
Michael B <mjbradvica@gmail.com>
2025-06-30 15:11:46 -0400

    add sass and bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { App } from "./app/app";
 import { homeRouteObject } from "./features/home/home.routes";
+import "./styles/index.scss";
 
 const router = createBrowserRouter([
   {
