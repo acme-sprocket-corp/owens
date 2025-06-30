e875fc3c92cbccd7990a3361c86ff47ca320d023
Michael Bradvica <mjbradvica@gmail.com>
2025-06-30 13:57:13 -0400

    linter updates

--- eslint.config.js
+++ eslint.config.js
@@ -8,16 +8,21 @@ import { globalIgnores } from "eslint/config";
 export default tseslint.config([
   globalIgnores(["dist"]),
   {
+    ignores: ["vitest.config.ts"],
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
 ]);
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app/app.tsx";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root") ?? new Element()).render(
   <StrictMode>
     <App />
   </StrictMode>,
